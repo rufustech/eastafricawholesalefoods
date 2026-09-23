bf96f9cc24d9cf719ab7637184c82d51b5403608
rufustech <mucherinvest@gmail.com>
2026-09-23 16:23:47 -0600

    TAG added google

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -85,6 +85,18 @@ export default function RootLayout({
       className="scroll-smooth bg-[#f8f2e5] dark:bg-[#0f2a1d]"
     >
       <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NDFMNF29');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
+
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#0ea5e9" />
@@ -128,6 +140,17 @@ export default function RootLayout({
         />
       </head>
       <body className="min-h-screen bg-[#f8f2e5] text-[#173b2b] dark:bg-[#0f2a1d] dark:text-[#f8f2e5]">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NDFMNF29"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
         <ThemeProvider>
           <ConstructionGate>
             <>
